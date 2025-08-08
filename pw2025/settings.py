cca65838e75ec2b94ea749345ab99c234e0aae58
Rafael Zottesso <rafael.zottesso@ifpr.edu.br>
2025-08-07 22:06:09 -0300

    Atualização do requirements e banco de dados para Supabase (postgres)

--- pw2025/settings.py
+++ pw2025/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'pw2025.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "postgres",
-#         "USER": "postgres.uusclivdiarwpamacsvo",
-#         "PASSWORD": "aaaa",
-#         "HOST": "aws-0-us-east-1.pooler.supabase.com",
-#         "PORT": "5432",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres.uusclivdiarwpamacsvo",
+        "PASSWORD": "b6mB3RLSX6x4YSKb",
+        "HOST": "aws-0-us-east-1.pooler.supabase.com",
+        "PORT": "5432",
+    }
+}
 
 # Sqlite local
 # DATABASES = {
@@ -96,19 +96,19 @@ WSGI_APPLICATION = 'pw2025.wsgi.application'
 # }
 
 # Mysql do PythonAnywhere
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'rafaelzottesso$default',
-        'USER': 'rafaelzottesso',
-        'PASSWORD': 'PW2025_mysql_xyz',
-        'HOST': 'rafaelzottesso.mysql.pythonanywhere-services.com',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
-        },
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'rafaelzottesso$default',
+#         'USER': 'rafaelzottesso',
+#         'PASSWORD': 'PW2025_mysql_xyz',
+#         'HOST': 'rafaelzottesso.mysql.pythonanywhere-services.com',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+#         },
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
