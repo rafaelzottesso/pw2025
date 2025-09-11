a9de764c3b3146712872fdc3b2f829d4f11f610f
Rafael Zottesso <rafael.zottesso@ifpr.edu.br>
2025-09-11 08:59:26 -0300

    Link para histórico list no menu

--- paginasweb/admin.py
+++ paginasweb/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Aluno, Campus, Curso, Historico, Servidor, Solicitacao, Status, TipoSolicitacao
 
 # Register your models here.
+admin.site.register(Aluno)
+admin.site.register(Campus)
+admin.site.register(Curso)
+admin.site.register(Historico)
+admin.site.register(Servidor)
+admin.site.register(Solicitacao)
+admin.site.register(Status)
+admin.site.register(TipoSolicitacao)
